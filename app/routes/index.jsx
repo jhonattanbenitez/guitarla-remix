a13c1d75fadd0df010c4b7522e769b6d584d1c8e
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-03-16 22:34:16 -0500

    index

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,4 +1,20 @@
+import {getGuitars} from '../models/guitars.server'
+import {getPosts} from '../models/post.server'
+export function meta() {
+  return {
+    title: 'Home'
+  }
+}
 
+export async function loader() {
+  const [guitars, posts] = await Promise.all([
+    getGuitars(),
+    getPosts()
+  ])
+  console.log(guitars)
+  console.log(posts)
+  return {}
+}
 function Index() {
   return (
     <div>Desde index</div>
